a74139885f9757062c02e858678e0c7c629d67f3
wong0846261 <136045756+wong0846261@users.noreply.github.com>
2023-06-09 12:32:55 +0800

    Update Program.cs

--- githubDemo/githubDemo/Program.cs
+++ githubDemo/githubDemo/Program.cs
@@ -21,6 +21,7 @@ class Program
 
     public int Max(int a, int b)
     {
+        //return a > a ? a : b;
         return a > a ? a : b;
     }
 
@@ -42,13 +43,14 @@ class Program
         foreach (int i in nr) // Bad multiple iteration
             Console.WriteLine(i);
 
-        int compareValue = pr.Max(3, 64); // Constant condition
+        // int compareValue = pr.Max(3, 64); // Useless assignment to local variable
 
         int valueA = 83;
-        int valueB = (int)valueA;
-        bool valueBoolean = checkBoolean(true, false, false, true, false, true, true); // Complex condition
+        int valueB = valueA;
+        // int valueB = (int)valueA;
+        // bool valueBoolean = checkBoolean(true, false, false, true, false, true, true); // Complex condition
         int rslt = pr.square(2); //Calling the method and assigning the value to an integer type
         Console.WriteLine("Square of the given number is  " + rslt + " boolean " + valueBoolean + " int " + valueB); //Printing the result
-        GC.Collect(); // Call to GC.Collect()
+        // GC.Collect(); // Call to GC.Collect()
     }
 }
